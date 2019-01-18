631243814d9887b82287416a1302befb677a4399
Imanol Eguskiza <ier@opendeusto.es>
2019-01-18 12:23:47 +0100

    OPENEUROPA-1503: Replace drupalcontext step to make it more readable.

--- tests/Behat/ContentLockContext.php
+++ tests/Behat/ContentLockContext.php
@@ -12,6 +12,39 @@ use Drupal\DrupalExtension\Context\RawDrupalContext;
 class ContentLockContext extends RawDrupalContext {
 
   /**
+   * Creates content authored by the current user.
+   *
+   * @Given I am viewing my :type_name content titled :title
+   */
+  public function createMyNode($type_name, $title): void {
+    if ($this->getUserManager()->currentUserIsAnonymous()) {
+      throw new \Exception(sprintf('There is no current logged in user to create a node for.'));
+    }
+
+    $content_type = \Drupal::entityTypeManager()
+      ->getStorage('node_type')
+      ->loadByProperties(['name' => $type_name]);
+
+    if (empty($content_type)) {
+      if ($this->getUserManager()->currentUserIsAnonymous()) {
+        throw new \Exception(sprintf('No %s content type found.', $type_name));
+      }
+    }
+    /** @var \Drupal\node\Entity\NodeType $content_type */
+    $content_type = reset($content_type);
+    $node = (object) [
+      'title' => $title,
+      'type' => $content_type->get('type'),
+      'body' => $this->getRandom()->name(255),
+      'uid' => $this->getUserManager()->getCurrentUser()->uid,
+    ];
+    $saved = $this->nodeCreate($node);
+
+    // Set internal page on the new node.
+    $this->getSession()->visit($this->locatePath('/node/' . $saved->nid));
+  }
+
+  /**
    * Install the content_lock component.
    *
    * @BeforeScenario @content_lock
--- tests/features/content_lock.feature
+++ tests/features/content_lock.feature
@@ -7,7 +7,7 @@ Feature: Content lock
   @content_lock
   Scenario: Content gets lock and lock can be broken
     Given I am logged in as a user with the "Author" role
-    And I am viewing my "oe_workflow_demo" with the title "Demo site"
+    And I am viewing my "Demo" content titled "Demo site"
     When I click "Edit"
     Then I should see "This content is now locked against simultaneous editing. This content will remain locked if you navigate away from this page without saving or unlocking it."
     # And I can unlock the node.
