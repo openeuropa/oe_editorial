400a27b995794b88c7e3d984cd8ee85ccff6d730
Imanol Eguskiza <ier@opendeusto.es>
2019-02-08 15:23:27 +0100

    OPENEUROPA-1503: Extend the behat test.

--- tests/Behat/ContentLockContext.php
+++ tests/Behat/ContentLockContext.php
@@ -4,15 +4,19 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_editorial\Behat;
 
-use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Drupal\DrupalExtension\Context\DrupalContext;
+use PHPUnit\Framework\Assert;
 
 /**
  * Defines step definitions for testing the Content Lock feature.
  */
-class ContentLockContext extends RawDrupalContext {
+class ContentLockContext extends DrupalContext {
 
   /**
    * Creates content authored by the current user.
+   *
+   * @throws \Exception
+   *   Throws an exception when the content type or the node are not found.
    *
    * @Given I am viewing my :type_name content titled :title
    */
@@ -21,18 +25,10 @@ class ContentLockContext extends RawDrupalContext {
       throw new \Exception('There is no current logged in user to create a node for.');
     }
 
-    $content_type = \Drupal::entityTypeManager()
-      ->getStorage('node_type')
-      ->loadByProperties(['name' => $type_name]);
-
-    if (empty($content_type)) {
-      throw new \Exception(sprintf('No %s content type found.', $type_name));
-    }
-    /** @var \Drupal\node\Entity\NodeType $content_type */
-    $content_type = reset($content_type);
+    $content_type = $this->getContentTypeFromName($type_name);
     $node = (object) [
       'title' => $title,
-      'type' => $content_type->get('type'),
+      'type' => $content_type,
       'body' => $this->getRandom()->name(255),
       'uid' => $this->getUserManager()->getCurrentUser()->uid,
     ];
@@ -40,6 +36,58 @@ class ContentLockContext extends RawDrupalContext {
 
     // Set internal page on the new node.
     $this->getSession()->visit($this->locatePath('/node/' . $saved->nid));
+  }
+
+  /**
+   * Navigates to a particular node..
+   *
+   * @param string $type_name
+   *   The content type of the node.
+   * @param string $title
+   *   The title of the node.
+   *
+   * @throws \Exception
+   *   Thrown when node is not found.
+   *
+   * @Then I visit the :type_name content titled :title
+   */
+  public function visitContentWithTitle($type_name, $title): void {
+
+    $content_type = $this->getContentTypeFromName($type_name);
+
+    $nodes = \Drupal::entityTypeManager()
+      ->getStorage('node')
+      ->loadByProperties(['title' => $title]);
+    /** @var \Drupal\node\Entity\Node $node */
+    foreach ($nodes as $node) {
+      if ($node->getType() == $content_type) {
+        $found = $node;
+        break;
+      }
+    }
+    if (empty($found)) {
+      throw new \Exception(sprintf('No %s content type with title %s found.', $type_name, $title));
+    }
+    // Set internal page on the new node.
+    $this->getSession()->visit($this->locatePath('/node/' . $found->id()));
+  }
+
+  /**
+   * Asserts the button with specified id|name|title|alt|value is disabled.
+   *
+   * @throws \Exception
+   *   Thrown when the button is not found.
+   *
+   * @Then the :button button is disabled
+   */
+  public function buttonIsDisabled($button) {
+    $button_element = $this->getSession()->getPage()->findButton($button);
+    if (empty($button_element)) {
+      throw new \Exception(sprintf('Button %s was not found.', $button));
+    }
+    $disabled = $button_element->getAttribute('disabled');
+    // Need to remove the label from the string.
+    Assert::assertEquals($disabled, 'disabled', 'Disabled state');
   }
 
   /**
@@ -66,6 +114,44 @@ class ContentLockContext extends RawDrupalContext {
     foreach ($modules as $module) {
       \Drupal::service('module_installer')->uninstall([$module]);
     }
+  }
+
+  /**
+   * Remove any created nodes.
+   *
+   * @AfterScenario
+   */
+  public function cleanNodes(): void {
+    if (\Drupal::moduleHandler()->moduleExists('content_lock')) {
+      \Drupal::database()->delete('content_lock')
+        ->execute();
+    }
+    parent::cleanNodes();
+  }
+
+  /**
+   * Returns the machine name of a content type from the readable name.
+   *
+   * @param string $type_name
+   *   Readable name of the content type.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   *
+   * @return string
+   *   Machine name of the content type.
+   */
+  private function getContentTypeFromName($type_name): string {
+    $content_type = \Drupal::entityTypeManager()
+      ->getStorage('node_type')
+      ->loadByProperties(['name' => $type_name]);
+
+    if (empty($content_type)) {
+      throw new \Exception(sprintf('No %s content type found.', $type_name));
+    }
+    /** @var \Drupal\node\Entity\NodeType $content_type */
+    $content_type = reset($content_type);
+    return (string) $content_type->get('type');
   }
 
 }
--- tests/features/content_lock.feature
+++ tests/features/content_lock.feature
@@ -5,12 +5,18 @@ Feature: Content lock
   so that no work is lost
 
   @content_lock
-  Scenario: Content gets lock and lock can be broken
+  Scenario: Content gets lock and another user cannot edit it until the lock is broken.
     Given I am logged in as a user with the "Author" role
     And I am viewing my "Demo" content titled "Demo site"
     When I click "Edit"
     Then I should see "This content is now locked against simultaneous editing. This content will remain locked if you navigate away from this page without saving or unlocking it."
-    # And I can unlock the node.
-    And I click "Unlock"
+    # Another user can't edit it.
+    Given I am logged in as a user with the "bypass node access, break content lock" permission
+    And I visit the "Demo" content titled "Demo site"
+    And I click "Edit"
+    Then the "Save" button is disabled
+    # After breaking the lock the content can be saved.
+    When I click "Break lock"
     And I press "Confirm break lock"
     Then I should see "Lock broken. Anyone can now edit this content."
+    And I press "Save"
