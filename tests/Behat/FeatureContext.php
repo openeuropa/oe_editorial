bf0719b2fd61c6705e3090307fae8e6cf9931bb9
nagyad <joevagyok@gmail.com>
2019-05-20 10:31:22 +0200

    OPENEUROPA-1770: Unused statement.

--- tests/Behat/FeatureContext.php
+++ tests/Behat/FeatureContext.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_editorial\Behat;
 
-use Behat\Behat\Tester\Exception\PendingException;
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 use Behat\Mink\Exception\ExpectationException;
 use Behat\Mink\Element\NodeElement;
