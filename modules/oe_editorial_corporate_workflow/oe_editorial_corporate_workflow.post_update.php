daec1e70457b1e1f01a8d7564786ede80cc458f7
nagyad <joevagyok@gmail.com>
2019-05-24 10:36:08 +0200

    OPENEUROPA-1770: Move update to post update.

--- modules/oe_editorial_corporate_workflow/oe_editorial_corporate_workflow.post_update.php
+++ modules/oe_editorial_corporate_workflow/oe_editorial_corporate_workflow.post_update.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * OpenEuropa Editorial Corporate Workflow install.
+ * OpenEuropa Editorial Corporate Workflow post updates.
  */
 
 declare(strict_types = 1);
@@ -12,7 +12,7 @@ use Drupal\user\Entity\Role;
 /**
  * Add missing permission for Validator.
  */
-function oe_editorial_corporate_workflow_update_8001(array &$sandbox): void {
+function oe_editorial_corporate_workflow_post_update_set_validator_permission(array &$sandbox): void {
   $role = Role::load('oe_validator');
   $role
     ->grantPermission('use oe_corporate_workflow transition validated_to_draft')
